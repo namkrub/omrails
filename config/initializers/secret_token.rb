2609f80c804ea57e53f70d8a4624d46a1f057cc3
Noel Burkman <namkrub@gmail.com>
2013-09-30 10:58:59 -0500

    Add Bootstarp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '1d24c8bd48f32bed5bc4aae3a91951a437636c986bb58630ca9d3d3fd74d62444be23b333bab99209b01cf4f9539b490965040df123bdbb1547f19c1d2b15c66'
